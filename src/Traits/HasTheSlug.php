0d9b0a225f47b1d9ea2f975144e83fcc1a18c4c4
Achyut Neupane <achyutkneupane@gmail.com>
2025-07-02 16:07:36 +0545

    No sluggableColumn in Trait

--- src/Traits/HasTheSlug.php
+++ src/Traits/HasTheSlug.php
@@ -9,14 +9,6 @@ trait HasTheSlug
     use Sluggable;
 
     /**
-     * Column name to generate the slug from.
-     * You can override this in your model.
-     *
-     * @var string
-     */
-    protected string $sluggableColumn = 'title';
-
-    /**
      * Get the configuration array for sluggable.
      */
     public function sluggable(): array
